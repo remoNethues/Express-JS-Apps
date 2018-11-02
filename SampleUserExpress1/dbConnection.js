6a1df1efa5c06002f03e6a5653d68b7db49c48a3
remoNethues <remo@123789.org>
2018-11-02 17:12:45 +0530

    used response.json

--- SampleUserExpress1/dbConnection.js
+++ SampleUserExpress1/dbConnection.js
@@ -8,19 +8,28 @@ function Connect(response) {
         console.log("from dbConnection");
         var dbo = data.db("mydb");
         console.log("from dbo");
-        var user1={
-            name:"rahul",
-            age:22
-        };
-        dbo.collection("users").insertOne(user1,function(err,response){
+        var user1=[
+            {name:"rahul", age:22},
+            {name:"hello", age:21},
+            {name:"Gaurav", age:23}
+        ];
+        dbo.collection("users").insertMany(user1,function(err,response){
             if(err) throw err;
-            console.log("data inserted");
+            console.log("data inserted"+response.insertedCount);
         });
         
         dbo.collection("users").find({}).toArray(function (err, result) {
             if (err) throw err;
             // console.log(result[0].age);
-            response.send(result[0].name+result[0].age);
+ 
+            response.json(result);
+            
+            // response.json(result.name + " " + result.age);
+            // var i;
+            // for (i in result){
+            //     response.send(result[i].name+result[i].age);
+
+            // };
             data.close();
         });                   
     });
