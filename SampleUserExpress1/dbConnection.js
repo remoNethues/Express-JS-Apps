8381d53900c3cf99698d7a7a2d7f670e22044361
remoNethues <remo@123789.org>
2018-11-02 17:47:56 +0530

    .

--- SampleUserExpress1/dbConnection.js
+++ SampleUserExpress1/dbConnection.js
@@ -1,6 +1,6 @@
 var client = require('mongodb').MongoClient;
 
-var url = "mongodb://localhost:27017/Nethues";
+var url = "mongodb://localhost:27017";
 
 function Connect(response) {
     client.connect(url, { useNewUrlParser: true }, function (err, data) {
@@ -8,33 +8,30 @@ function Connect(response) {
         console.log("from dbConnection");
         var dbo = data.db("mydb");
         console.log("from dbo");
-        var user1=[
-            {name:"rahul", age:22},
-            {name:"hello", age:21},
-            {name:"Gaurav", age:23}
-        ];
-        dbo.collection("users").insertMany(user1,function(err,response){
-            if(err) throw err;
-            console.log("data inserted"+response.insertedCount);
-        });
-        
+        // var user1 = [
+        //     { name: "rahul", age: 22 },
+        //     { name: "hello", age: 21 },
+        //     { name: "Gaurav", age: 23 }
+        // ];
+        // dbo.collection("users").insertMany(user1, function (err, response) {
+        //     if (err) throw err;
+        //     console.log("data inserted" + response.insertedCount);
+        // });        
         dbo.collection("users").find({}).toArray(function (err, result) {
             if (err) throw err;
             // console.log(result[0].age);
  
             response.json(result);
-            
+
             // response.json(result.name + " " + result.age);
             // var i;
             // for (i in result){
-            //     response.send(result[i].name+result[i].age);
-
+            //     response.json(result[i].name+result[i].age);
             // };
             data.close();
         });                   
     });
 }
-
 module.exports={
     Connect
 }
